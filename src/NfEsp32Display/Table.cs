1a4f2e3a47f79cc999383bbcc4c386d3adbcfba7
evjenio <evjenio@ya.ru>
2022-05-02 02:10:23 +0300

    Removed unused

--- src/NfEsp32Display/Table.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NfEsp32Display
-{
-    public static class Table
-    {
-        public static string Row(string arg0, string arg1, string arg2, string arg3) => string.Format("|{0,-30}|{1,-17}|{2,4}|{3,6}|", arg0, arg1, arg2, arg3);
-    }
-}
